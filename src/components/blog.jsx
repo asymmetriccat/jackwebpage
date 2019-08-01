23e114583b5f23ba495a3fc63965c2f0e16a745e
guilongli <jli@crmsuite.com>
2019-07-31 22:28:27 -0500

    update blog 7/31/19

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 
 export default class Blog extends Component {
     render() {
+
         return (
             <div>
                 <section className="colorlib-blog" data-section="blog">
@@ -34,6 +35,67 @@ export default class Blog extends Component {
                                 </div>
                             </div>
                         </div>
+
+                        <div className="row">
+                            <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+                                <div className="blog-entry">
+                                    {/*<a href="blog.html" className="blog-img"><img src="images/blog-1.jpg"
+                                                                                  className="img-responsive"
+                                                                                  alt="HTML5 Bootstrap Template by colorlib.com"/></a>*/}
+                                    <div className="desc">
+                                        <span><small>July 31, 2019 </small> | <small> java </small> | <small> <i
+                                            className="icon-bubble3"/> 4</small></span>
+                                        <h3><a href="https://leetcode.com/problems/longest-substring-without-repeating-characters/"> Leetcode problems: longest-substring-without-repeating-characters </a>
+                                        </h3>
+                                        <p> Today I am starting to solve leetcode problems without using IDE. Below is the snapshot of the solution for the titled problem.
+                                            <img src="images/2019-07-31_1.png"/>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+                                <div className="blog-entry">
+                                    {/*<a href="blog.html" className="blog-img"><img src="images/blog-1.jpg"
+                                                                                  className="img-responsive"
+                                                                                  alt="HTML5 Bootstrap Template by colorlib.com"/></a>*/}
+                                    <div className="desc">
+                                        <span><small>July 31, 2019 </small> | <small> java </small> | <small> <i
+                                            className="icon-bubble3"/> 4</small></span>
+                                        <h3><a href="https://leetcode.com/problems/add-two-numbers/">  Leetcode problems: Add two numbers</a>
+                                        </h3>
+                                        <p>
+                                            <img src="images/2019-07-31_2.png"/>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+                                <div className="blog-entry">
+                                    {/*<a href="blog.html" className="blog-img"><img src="images/blog-1.jpg"
+                                                                                  className="img-responsive"
+                                                                                  alt="HTML5 Bootstrap Template by colorlib.com"/></a>*/}
+                                    <div className="desc">
+                                        <span><small>July 31, 2019 </small> | <small> java </small> | <small> <i
+                                            className="icon-bubble3"/> 4</small></span>
+                                        <h3><a href="https://leetcode.com/problems/most-common-word/">  Leetcode problems: Most common word</a>
+                                        </h3>
+                                        <p>
+                                            <img src="images/2019-07-31_3.png"/>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
                         <div className="row">
                             <div className="col-md-12 animate-box">
                                 <p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i
